a80c0267643ad8a7541a4fff9f128fa95380fef6
kashw2 <supra4keanu@hotmail.com>
2022-07-10 21:55:06 +1000

    fix(terraform): limit throughput to 1000 RU

--- terraform/modules/cosmos/database/database.tf
+++ terraform/modules/cosmos/database/database.tf
@@ -2,4 +2,5 @@ resource "azurerm_cosmosdb_sql_database" "database" {
   account_name        = var.account_name
   name                = "quickstack"
   resource_group_name = var.resource_group_name
+  throughput          = 1000
 }
