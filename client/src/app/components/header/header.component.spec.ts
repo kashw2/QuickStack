0ac6c164c78a301d62fa05cd7213419d5d73e21b
kashw2 <supra4keanu@hotmail.com>
2022-07-13 19:07:53 +1000

    test(client): added az auth requirements

--- client/src/app/components/header/header.component.spec.ts
+++ client/src/app/components/header/header.component.spec.ts
@@ -1,6 +1,70 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import {HeaderComponent} from './header.component';
+import {BrowserCacheLocation, InteractionType, IPublicClientApplication, LogLevel, PublicClientApplication} from "@azure/msal-browser";
+import {environment} from "../../../environments/environment";
+import {
+  MSAL_GUARD_CONFIG,
+  MSAL_INSTANCE,
+  MSAL_INTERCEPTOR_CONFIG,
+  MsalBroadcastService,
+  MsalGuard,
+  MsalGuardConfiguration,
+  MsalInterceptor,
+  MsalInterceptorConfiguration,
+  MsalModule,
+  MsalService
+} from "@azure/msal-angular";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1; // Remove this line to use Angular Universal
+
+function loggerCallback(logLevel: LogLevel, message: string) {
+  console.log(message);
+}
+
+function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: environment.AZURE_CLIENT_ID,
+      authority: environment.AZURE_AUTHORITY,
+      knownAuthorities: [environment.AZURE_AUTHORITY_DOMAIN],
+      redirectUri: '/',
+      postLogoutRedirectUri: '/'
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+      storeAuthStateInCookie: isIE, // set to true for IE 11. Remove this line to use Angular Universal
+    },
+    system: {
+      loggerOptions: {
+        loggerCallback,
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false
+      }
+    }
+  });
+}
+
+function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set(`https://${environment.AZURE_AUTHORITY_DOMAIN}/`, ['user.read']);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap
+  };
+}
+
+function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: [`https://${environment.AZURE_AUTHORITY_DOMAIN}/user.read`]
+    },
+    loginFailedRoute: '/login-failed'
+  };
+}
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,9 +72,32 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent],
+      imports: [MsalModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: MsalInterceptor,
+          multi: true
+        },
+        {
+          provide: MSAL_INSTANCE,
+          useFactory: MSALInstanceFactory
+        },
+        {
+          provide: MSAL_GUARD_CONFIG,
+          useFactory: MSALGuardConfigFactory
+        },
+        {
+          provide: MSAL_INTERCEPTOR_CONFIG,
+          useFactory: MSALInterceptorConfigFactory
+        },
+        MsalService,
+        MsalGuard,
+        MsalBroadcastService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
